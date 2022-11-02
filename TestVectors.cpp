0932a784525e9198aa0a669669423a0e72f475b1
Kamel R <kamel.r@wayne.edu>
2022-11-02 17:59:00 -0400

    Add project files.

---
+++ TestVectors.cpp
@@ -0,0 +1,27 @@
+// TestVectors.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int main()
+{
+    vector<int> vec1;
+
+    
+
+    for (int i = 1; i <= 5; i++)
+    {
+        vec1.push_back(i);
+    }
+
+    for (int i = 0; i <vec1.size(); i++)
+    {
+        cout<<vec1[i]<<endl;
+    }
+
+    cout << vec1.size() << endl;
+
+    //vector<char> vec2 (10,)
+}
